057e27247d245942201546522044b2d0889f10fc
Peter Do <dophatgl@gmail.com>
2024-12-05 16:26:55 +0700

    Create setup-repo.sh

---
+++ setup-repo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+echo "setup ccminer program!"
+apt-get update -y
+apt-get upgrade -y
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git nano  cmake -y
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer
+cd ccminer
+chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh
+./build.sh
+cd ..
